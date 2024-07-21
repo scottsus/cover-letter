828e17cda223d69aed3029cb6e9d9b0af37dcfc7
Scott Susanto <scott@cofactory.ai>
2024-07-20 23:32:20 -0700

    40s server action generation timeout

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -4,6 +4,8 @@ import { Navbar } from "~/components/navbar";
 import { MainArea } from "./main";
 import { getPageHits } from "~/actions/analytics";
 
+export const maxDuration = 40;
+
 export default async function HomePage() {
   const numHits = await getPageHits();
 
