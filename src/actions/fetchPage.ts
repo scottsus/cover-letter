6a8f4123d0bc416e2701f0934e8b73eab63bcbd3
scott susanto <susantoscott@gmail.com>
2024-08-22 12:03:55 -0700

    playwright in dev mode

--- src/actions/fetchPage.ts
+++ src/actions/fetchPage.ts
@@ -1,4 +1,5 @@
 import { generateText } from "ai";
+import { chromium } from "playwright";
 
 import { getInnerText } from "~/lib/cheerio";
 import { DEFAULT_MODEL } from "~/lib/models";
@@ -11,19 +12,41 @@ export async function getPageContents({
   url: string;
   enrich?: boolean;
 }) {
-  const htmlSoup = await fetch(url)
-    .then((res) => res.text())
-    .catch((err) => console.error(err));
-  const text = getInnerText(htmlSoup ?? "");
-  if (!enrich) {
-    return text;
+  let pageContents = "";
+  if (process.env.NODE_ENV === "development") {
+    /**
+     * In dev, we can launch a playwright chromium instance
+     */
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    await page.waitForLoadState("networkidle");
+
+    const content = await page.content();
+    await browser.close();
+
+    pageContents = getInnerText(content);
+  } else {
+    /**
+     * In prod, do a best effort reading of the url
+     */
+    const htmlSoup = await fetch(url)
+      .then((res) => res.text())
+      .catch((err) => console.error(err));
+
+    pageContents = getInnerText(htmlSoup ?? "");
   }
-  console.log("Text:", text);
+
+  console.log("PageContents:", pageContents);
+  if (!enrich) {
+    return pageContents;
+  }
 
   const jobPostingContents = await generateText({
     model: DEFAULT_MODEL,
     system: htmlPageReconstruction,
-    prompt: text,
+    prompt: pageContents,
   });
 
   return jobPostingContents.text;
