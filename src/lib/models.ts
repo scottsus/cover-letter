7291251d65430b42f08379de27f2df9ef1f553d5
scott susanto <susantoscott@gmail.com>
2024-08-22 13:07:41 -0700

    use latest 4o model

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -2,7 +2,7 @@ import { anthropic } from "@ai-sdk/anthropic";
 import { openai } from "@ai-sdk/openai";
 
 export const models = {
-  openai: openai("gpt-4o"),
+  openai: openai("gpt-4o-2024-08-06"),
   "claude-3-5-sonnet": anthropic("claude-3-5-sonnet-20240620"),
 };
 
