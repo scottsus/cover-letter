30a69613618583df764e7a7d068ce1a3cda68037
scott susanto <susantoscott@gmail.com>
2024-08-22 18:29:26 -0700

    force-dynamic on /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from "next/navigation";
 
 import { recordPageHit } from "~/actions/analytics";
 
+export const dynamic = "force-dynamic";
+
 export default function HomePage() {
   recordPageHit();
   redirect("/generate");
